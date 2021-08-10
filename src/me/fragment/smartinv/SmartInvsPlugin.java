aafbbe5825e0d2aa736c765c308863c2fa19ac68
Fragment <clemcailly@gmail.com>
2021-08-10 20:30:48 +0200

    v1

---
+++ src/me/fragment/smartinv/SmartInvsPlugin.java
@@ -0,0 +1,21 @@
+package me.fragment.smartinv;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SmartInvsPlugin extends JavaPlugin {
+
+    private static SmartInvsPlugin instance;
+    private static InventoryManager invManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        invManager = new InventoryManager(this);
+        invManager.init();
+    }
+
+    public static InventoryManager manager() { return invManager; }
+    public static SmartInvsPlugin instance() { return instance; }
+
+}
